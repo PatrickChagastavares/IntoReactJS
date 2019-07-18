ba22fbb7348076cadfb8726284d37251119179bf
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-07-18 08:47:51 -0300

    create: configuring text input and changing it's status

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,21 +1,48 @@
 import React, { Component } from "react";
 
 class TechList extends Component {
-state = {
-  techs: [
-    "node.js",
-    "ReactJS",
-    "React Native"
-  ]
-}
+  state = {
+    NewTech: "",
+    techs: ["node.js", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ NewTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.NewTech],
+      NewTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(Tec => Tec !== tech) });
+  };
 
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.NewTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
